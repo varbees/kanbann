2e0756ff96b7812cb5fe84db511f2df7480540cd
varbees <harshavar968@gmail.com>
2023-10-28 16:32:33 +0530

    Lock Button UI Setup

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const modalContainer = document.querySelector('.modal-container');
+const colorBtns = document.querySelectorAll('.color');
 const addBtn = document.querySelector('.add-btn');
+const lockIcon = document.getElementById('lock-icon');
+
+colorBtns.forEach(colorBtn => {
+  colorBtn.addEventListener('click', () => {
+    console.log(colorBtn.classList[0]);
+    removeAllTickets();
+  });
+});
 
 let isModalTaskOpen = false;
+addBtn.addEventListener('click', () =>
+  !isModalTaskOpen ? displayModal() : hideModal()
+);
+
+modalOverlay.addEventListener('click', () => {
+  hideModal();
+});
+
+let isLocked = true;
+lockIcon.addEventListener('click', () => {
+  if (isLocked) {
+    lockIcon.classList.remove('fa-lock');
+    lockIcon.classList.add('fa-unlock');
+    isLocked = false;
+  } else {
+    lockIcon.classList.add('fa-lock');
+    lockIcon.classList.remove('fa-unlock');
+    isLocked = true;
+  }
+});
+
+function removeAllTickets() {
+  const mainContainer = document.querySelector('.main-container');
+  while (mainContainer.firstChild) {
+    mainContainer.removeChild(mainContainer.firstChild);
+  }
+}
 
 function displayModal() {
   modalOverlay.style.display = 'block';
@@ -15,11 +51,3 @@ function hideModal() {
   modalOverlay.style.display = 'none';
   isModalTaskOpen = false;
 }
-
-addBtn.addEventListener('click', () =>
-  !isModalTaskOpen ? displayModal() : hideModal()
-);
-
-modalOverlay.addEventListener('click', () => {
-  hideModal();
-});
